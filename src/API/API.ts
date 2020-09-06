ffe9cb1417ab4b6707661be7358fdc264106e304
Alain Afonso <alainpedro123@gmail.com>
2020-09-06 09:55:04 +0200

    adding comments

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,29 +1,36 @@
-import { shuffleArray } from './../utils/utils';
+import { shuffleArray } from "./../utils/utils";
 
 export type Question = {
-    category: string;
-    correct_answer: string;
-    difficulty: string;
-    incorrect_answers: string[];
-    question: string;
-    type: string;
-}
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
 
+// this "QuestionState" type below will use the properties from the "Question" type above plus the property "answers: string[]"
 export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
-    EASY = "easy",
-    MEDIUM = "medium",
-    HARD = "hard"
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
 }
 
-export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
-    const getRequest = await fetch(`https://opentdb.com/api.php?amount=${amount}&difficulty${difficulty}&type=multiple`);
-    const data = await getRequest.json();
-    return data.results.map((question: Question) => (
-        {
-            ...question,
-            answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
-        }
-    ))
-}
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const getRequest = await fetch(
+    `https://opentdb.com/api.php?amount=${amount}&difficulty${difficulty}&type=multiple`
+  );
+  const data = await getRequest.json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
